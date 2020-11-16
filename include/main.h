b121ef2c55bfd46f56be0165d525a3ce5afa7d2e
Origin <ruchdaneabodun@gmail.com>
2020-11-16 05:39:13 +0100

    L'ecran de base peut etre acceder de n'importe ou

---
+++ include/main.h
@@ -0,0 +1,20 @@
+#ifndef RUCHDANEMAIN
+#define RUCHDANEMAIN
+#define PIX 34
+#define TEST 0
+#define Failed_case -100
+#define FPS 1000 / 60
+
+#include <stdio.h>
+#include "stdlib.h"
+#include "SDL.h"
+#include "SDL_image.h"
+#include "SDL_ttf.h"
+#include "fonction.h"
+#include "structure.h"
+#include "rule.h"
+#include "ini.h"
+#include "Gui.h"
+SDL_Window *GetWindow();
+
+#endif
